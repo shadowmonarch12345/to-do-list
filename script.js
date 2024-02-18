e10e97964360734313bd21b9e61069583983146d
shadowmonarch12345 <138355840+shadowmonarch12345@users.noreply.github.com>
2024-02-18 15:26:16 +0530

    javascript added

--- script.js
+++ script.js
@@ -12,6 +12,27 @@ function addTask(){
         let span= document.createElement("span");
         span.innerHTML="\u00d7"
         li.appendChild(span)
+        savedata();
     }
     inputBox.value="";
 }
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        savedata()
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        savedata()
+    }
+},false);
+
+function savedata(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
